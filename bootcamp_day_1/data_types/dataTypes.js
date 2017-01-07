0b26494088ad85440ccba90933874760d5b85e29
koyexes <no-reply@email.com>
2017-01-07 22:37:52 +0100

    dataType completed completed the code implementation for data type task

--- bootcamp_day_1/data_types/dataTypes.js
+++ bootcamp_day_1/data_types/dataTypes.js
@@ -1,3 +1,31 @@
 /**
  * Created by koyexes on 1/7/2017.
  */
+'use strict'
+
+module.exports = {
+    dataTypes: function (parameter) {
+        var output;
+         switch (typeof parameter) {
+             case "string":
+                 output = parameter.length;
+                 break;
+             case "object":
+                 output = (parameter instanceof Array) ? (parameter.length > 2 ?  parameter.slice(0,4).pop() : undefined) : "no value";
+                 break;
+             case "undefined":
+                 output = "no value";
+                 break;
+             case "boolean":
+                 output = parameter;
+                 break;
+             case "number":
+                 output = (parameter > 100) ? "more than 100" : ((parameter < 100) ? "less than 100" : "equal to 100");
+                 break;
+             default:
+                 output = parameter(true);
+                 break;
+         }
+         return output;
+    }
+}
