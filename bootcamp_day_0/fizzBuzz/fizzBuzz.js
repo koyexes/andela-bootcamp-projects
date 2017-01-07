2e628a4434573b2247ca840ba563269fa704b4f3
koyexes <no-reply@email.com>
2017-01-07 08:54:24 +0100

    FizzBuzz task Completed

--- bootcamp_day_0/fizzBuzz/fizzBuzz.js
+++ bootcamp_day_0/fizzBuzz/fizzBuzz.js
@@ -6,8 +6,7 @@
 'use strict'
 module.exports = {
     fizzBuzz: (number) => {
-        return (((number % 3 == 0) && (number % 5 == 0)) ? 'FizzBuzz' : ((number % 3 == 0) ? "Fizz" : ((number % 5 == 0) ? 'Buzz' : number)))
-
+        return (((number % 3 == 0) && (number % 5 == 0)) ? 'FizzBuzz' : ((number % 3 == 0) ? "Fizz" : ((number % 5 == 0) ? 'Buzz' : number)));
     }
 
 }
