5c8a4a00e4997a5da4df4c81dbbdb598ee8967f8
koyexes <no-reply@email.com>
2017-01-07 10:36:37 +0100

    Re-implemented findMinMax codes re-implemented findMinMax codes to pass newly implemented tests

--- bootcamp_day_0/minimum_and_maximum/findMinMax.js
+++ bootcamp_day_0/minimum_and_maximum/findMinMax.js
@@ -6,21 +6,27 @@
 module.exports = {
     findMinMax : function (arrayOfNumbers) {
             var outputArray = [];
-            var minimumNumber = arrayOfNumbers[0];
-            var maximumNumber = arrayOfNumbers[0];
-            for (var index = 1; index < arrayOfNumbers.length; index++) {
-                minimumNumber = (arrayOfNumbers[index] < minimumNumber) ? arrayOfNumbers[index] : minimumNumber;
-                maximumNumber = (arrayOfNumbers[index] > maximumNumber) ? arrayOfNumbers[index] : maximumNumber;
+            try {
+                var minimumNumber = (typeof arrayOfNumbers[0] !== "number") ? parseInt(arrayOfNumbers[0]) : arrayOfNumbers[0];
+                var maximumNumber = (typeof arrayOfNumbers[0] !== "number") ? parseInt(arrayOfNumbers[0]) : arrayOfNumbers[0];
+                for (var index = 1; index < arrayOfNumbers.length; index++) {
+                    var num = (typeof arrayOfNumbers[index] !== "number" ) ? parseInt(arrayOfNumbers[index]) : arrayOfNumbers[index];
+                    minimumNumber = (num < minimumNumber) ? num : minimumNumber;
+                    maximumNumber = (num > maximumNumber) ? num : maximumNumber;
+                }
+                switch (minimumNumber === maximumNumber) {
+                    case true:
+                        outputArray.push(minimumNumber);
+                        break;
+                    default:
+                        outputArray.push(minimumNumber);
+                        outputArray.push(maximumNumber);
+                        break;
+                }
+            } catch (err) {
+                console.log(err);
             }
-            switch (minimumNumber === maximumNumber) {
-                case true:
-                    outputArray.push(minimumNumber);
-                    break;
-                default:
-                    outputArray.push(minimumNumber);
-                    outputArray.push(maximumNumber);
-                    break;
-            }
+
             return outputArray;
     }
 }
