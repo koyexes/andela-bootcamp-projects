1205583ab788fbda3836a414f45240736a086bf5
koyexes <no-reply@email.com>
2017-01-09 20:34:21 +0100

    Reverse String Logic Implemented

--- bootcamp_day_2/reverse_string/reverseString.js
+++ bootcamp_day_2/reverse_string/reverseString.js
@@ -5,3 +5,8 @@
 function helperFunction(parameter) {
     return (parameter.length === 0) ? "" : (helperFunction(parameter.substring(1)) + parameter.charAt(0));
 }
+
+function reverseString(string) {
+    var output = helperFunction(string.trim()); // calls the helper function as a callback
+    return (output.trim().length === 0) ? null : (output === string ? true : output);
+}
