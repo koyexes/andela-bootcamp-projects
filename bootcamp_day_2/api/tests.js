49f6f687cbe5dfe2f24852c05f84cb91f37db7cf
koyexes <no-reply@email.com>
2017-01-10 16:27:05 +0100

    Test updated

--- bootcamp_day_2/api/tests.js
+++ bootcamp_day_2/api/tests.js
@@ -3,22 +3,25 @@
  */
 'use strict'
 var geo = require('./coordinates');
+
 describe("Geographical coordinates", function() {
     var call = function (error, result) {
         if(error) {
             return undefined;
         }
     }
-
     it("should return undefined if error occurs for a boolean value", function () {
         expect(geo.geocodeAddress(true, call)).toEqual(undefined);
     });
-
     var fcall = function (error, result) {
             return result;
-    }
+    };
      it("should return undefined for result for a boolean value", function () {
         expect(geo.geocodeAddress(true, fcall)).toEqual(undefined);
+    });
+
+     it("should return undefined for value ',,,,,'", function () {
+        expect(geo.geocodeAddress(",,,,,", call)).toEqual(undefined);
     });
 
 });
