e75e24518a00c8c5f22e7d50a5300c24d7d1a21c
koyexes <no-reply@email.com>
2017-01-11 08:36:30 +0100

    Binary Implementation

---
+++ bootcamp_day_3/binary_search/binarySearch.js
@@ -0,0 +1,55 @@
+/**
+ * Created by koyexes on 1/10/2017.
+ */
+
+Array.prototype.toTwenty = () => Array.apply(undefined, Array(20)).map((value, index) => index + 1);
+Array.prototype.toForty = () => Array.apply(undefined, Array(20)).map((value, index) => (index * 2) + 2);
+Array.prototype.toOneThousand = () => Array.apply(undefined, Array(100)).map((value, index) => (index * 10) + 10);
+Array.prototype.search = function (number) {
+    var lowerLimit = 0;
+    var upperLimit = this.length - 1;
+    var midPoint;
+    var counter = 0;
+    var hasFoundIt = false;
+    switch (number <= this[lowerLimit] || number >= this[upperLimit]){
+        case false:
+             midPoint = Math.floor((upperLimit + lowerLimit) / 2);
+             while (lowerLimit <= upperLimit) {
+                midPoint = Math.floor((upperLimit + lowerLimit) / 2);
+                if (this[midPoint] === number) {
+                    hasFoundIt = true;
+                    break;
+                } else {
+                    if (number < this[midPoint]) {
+                        upperLimit = midPoint - 1;
+                    } else {
+                        lowerLimit = midPoint + 1;
+                    }
+                }
+                 ++counter;
+            }
+            midPoint = hasFoundIt ? midPoint : -1;
+            break;
+        default:
+            midPoint = (this[lowerLimit] === number) ? lowerLimit : (this[upperLimit] === number ? upperLimit : -1);
+            break;
+    }
+
+    return {
+                count: counter,
+                index: midPoint,
+                length: this.length
+            };
+};
+
+var array = [].toOneThousand();
+//console.log(array);
+console.log(array.search(40));
+//console.log([].toForty().search(30));
+//console.log([].toOneThousand().search(100));
+
+module.exports = {
+    toTwenty: Array.toTwenty,
+    toForty: Array.toForty,
+    toOneThousand: Array.toOneThousand
+}
